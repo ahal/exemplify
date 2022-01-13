37d3114d3cbc5d14363344a2c09281d4a8042040
Andrew Halberstadt <ahal@pm.me>
2022-01-12 23:45:48 -0500

    Support multiple packages at once in npm installable

--- wellington/installables/node.py
+++ wellington/installables/node.py
@@ -6,8 +6,11 @@ from wellington.installables.base import Installable, register
 
 @register("npm")
 class Npm(Installable):
-    def __init__(self, package, global_=True, npm_path=None):
-        self.package = package
+    def __init__(self, packages, global_=True, npm_path=None):
+        self.packages = packages
+        if isinstance(self.packages, str):
+            self.packages = [self.packages]
+
         npm_path = npm_path or "npm"
         self.npm = os.path.expanduser(npm_path)
 
@@ -17,16 +20,16 @@ class Npm(Installable):
 
     def exists(self):
         try:
-            subprocess.check_output([self.npm, "show", self.package])
+            subprocess.check_output([self.npm, "show"] + self.packages)
             return True
         except subprocess.CalledProcessError:
             return False
 
     def install(self):
-        subprocess.check_call(self.args + [self.package])
+        subprocess.check_call(self.args + self.packages)
 
     def update(self):
-        subprocess.check_call(self.args + ["--upgrade", self.package])
+        subprocess.check_call(self.args + ["--upgrade"] + self.packages)
 
     def __str__(self):
-        return "NPM INSTALL {}".format(self.package)
+        return "NPM INSTALL {}".format(" ".join(self.packages))
