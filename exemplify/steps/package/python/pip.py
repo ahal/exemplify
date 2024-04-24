1b06ba5c0e5f2a308559d58797d303a311538eaa
Andrew Halberstadt <ahal@pm.me>
2024-04-24 00:04:25 -0400

    fix(pip): always use --upgrade
    To restrict package versions, specifiers can be used alongside the
    package name.

--- exemplify/steps/package/python/pip.py
+++ exemplify/steps/package/python/pip.py
@@ -17,18 +17,8 @@ class Pip(Step):
         pip_path = pip_path or "~/.pyenv/shims/pip"
         self.pip = os.path.expanduser(pip_path)
 
-    def exists(self) -> bool:
-        try:
-            output = subprocess.check_output([self.pip, "list"], text=True)
-            return all(p in output for p in self.packages)
-        except subprocess.CalledProcessError:
-            return False
-
     def sync(self) -> None:
-        args = [self.pip, "install"]
-        if self.exists():
-            args.append("--upgrade")
-        subprocess.check_call(args + self.packages)
+        subprocess.check_call([self.pip, "install", "--upgrade"] + self.packages)
 
     def __str__(self):
         return f"PIP INSTALL {', '.join(self.packages)}"
