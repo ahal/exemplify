7ac18e0f8c632f328fb13531520e2542fa954225
Andrew Halberstadt <ahal@pm.me>
2024-04-25 23:19:45 -0400

    feat: implement rich progress

--- exemplify/cli.py
+++ exemplify/cli.py
@@ -1,9 +1,18 @@
 import os
 import sys
 from argparse import ArgumentParser
+from itertools import chain
 from pathlib import Path
 
-from exemplify.console import console
+from rich.console import Group
+from rich.live import Live
+from rich.progress import (
+    BarColumn,
+    Progress,
+    TextColumn,
+)
+
+from exemplify.console import RoutineProgress, console
 from exemplify import generate_steps, parse_config, synchronize
 
 
@@ -41,13 +50,72 @@ def run(args=sys.argv[1:]):
     if not routines:
         routines = [k for k in config.keys() if k != "meta"]
 
-    for routine in routines:
-        for step in generate_steps(routine, config):
-            with console.status(f"Processing routine {routine}: STEP {step}"):
+    routine_progress = []
+    num_steps = 0
+    for name in routines:
+        routine_progress.append(RoutineProgress(name))
+
+        for step in generate_steps(name, config):
+            num_steps += 1
+            routine_progress[-1].step.add_step(step)
+
+    overall_progress = Progress(
+        TextColumn("Exemplify"),
+        BarColumn(),
+        TextColumn("{task.description}"),
+    )
+    overall_task_id = overall_progress.add_task("", total=num_steps)
+
+    progress_group = Group(
+        *chain.from_iterable((rp, rp.step) for rp in routine_progress),
+        overall_progress,
+    )
+
+    with Live(
+        progress_group,
+        refresh_per_second=10,
+        redirect_stdout=False,
+        redirect_stderr=False,
+    ):
+        for idx, progress in enumerate(routine_progress):
+            top_description = (
+                f"[bold #AAAAAA]({idx} / {len(routine_progress)} routines complete)"
+            )
+            overall_progress.update(overall_task_id, description=top_description)
+
+            progress.start_routine()
+            progress.update(
+                description=f"[bold]Routine [blue]{progress.routine}[/blue] processing.."
+            )
+
+            while True:
+                try:
+                    step = next(progress.step)
+                except StopIteration:
+                    break
+
                 with console.capture() as capture:
                     ret = synchronize(step)
 
-                if ret == 0:
-                    console.print(f":white_heavy_check_mark: {step}")
+                progress.returncode |= ret
+                if ret != 0:
+                    lines = capture.get().splitlines()
+                    stdout = "\n".join(f"  {line}" for line in lines)
+                    description = f":x: {step}\n{stdout}"
+                    progress.step.update(description=description)
                 else:
-                    print(capture.get())
+                    progress.step.update(description=f":white_heavy_check_mark: {step}")
+
+                overall_progress.update(overall_task_id, advance=1)
+
+            progress.stop_routine()
+            if progress.returncode == 0:
+                progress.update(
+                    description=f"[bold green]Routine [blue]{progress.routine}[/blue] succeeded!",
+                )
+            else:
+                progress.update(
+                    description=f"[bold red]Routine [blue]{progress.routine}[/blue] failed!",
+                )
+
+        overall_progress.update(overall_task_id, visible=False)
--- exemplify/console.py
+++ exemplify/console.py
@@ -1,4 +1,98 @@
 from rich.console import Console
+from rich.progress import (
+    Progress,
+    SpinnerColumn,
+    TextColumn,
+    TimeElapsedColumn,
+)
+from rich.text import Text
+
+from exemplify.steps.base import Step
 
 
 console = Console()
+
+
+class HiddenSpinnerColumn(SpinnerColumn):
+    def __init__(self, text, **kwargs):
+        super().__init__(**kwargs)
+        self.text = text
+
+    def render(self, task):
+        text = self.text.format(task=task)
+
+        if task.started and not task.finished:
+            return f"{self.spinner.render(task.get_time())}  {text}"
+
+        if not task.started:
+            return f":heavy_minus_sign: {text}"
+
+        return text
+
+
+class HiddenTimeElapsedColumn(TimeElapsedColumn):
+    def render(self, task):
+        return super().render(task) if task.started and not task.finished else Text("")
+
+
+class StepProgress(Progress):
+    def __init__(self, **kwargs):
+        self._current = -1
+        self.steps = []
+        return super().__init__(
+            TextColumn(" "),
+            HiddenSpinnerColumn(
+                "[purple]{task.description}[/purple]", spinner_name="dots"
+            ),
+            **kwargs,
+        )
+
+    def add_step(self, step: Step) -> None:
+        task_id = self.add_task(str(step), start=False, visible=False)
+        self.steps.append((task_id, step))
+
+    def __next__(self) -> Step:
+        if self._current >= 0:
+            task_id, step = self.steps[self._current]
+            self.stop_task(task_id)
+            self.update(advance=1)
+
+        self._current += 1
+        try:
+            task_id, step = self.steps[self._current]
+        except IndexError:
+            raise StopIteration
+
+        self.update(total=1)
+        self.start_task(task_id)
+        return step
+
+    def start(self):
+        for task_id, _ in self.steps:
+            super().update(task_id, visible=True)
+
+    def update(self, *args, **kwargs):
+        task_id, _ = self.steps[self._current]
+        super().update(task_id, *args, **kwargs)
+
+
+class RoutineProgress(Progress):
+    def __init__(self, routine, **kwargs):
+        self.returncode = 0
+        self.routine = routine
+        self.step = StepProgress()
+        super().__init__(
+            TextColumn("{task.description}"), HiddenTimeElapsedColumn(), **kwargs
+        )
+
+        self._id = self.add_task(f"Routine {self.routine}", start=False)
+
+    def update(self, *args, **kwargs):
+        return super().update(self._id, *args, **kwargs)
+
+    def start_routine(self):
+        self.step.start()
+        self.start_task(self._id)
+
+    def stop_routine(self):
+        self.stop_task(self._id)
